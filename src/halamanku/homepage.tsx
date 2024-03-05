3785d99b3c992b2a6cd0a78a3e2f9d75da3a4c24
hernanhuwae <martinushuwae014@gmail.com>
2024-03-05 22:08:37 +0700

    inoicain

--- src/halamanku/homepage.tsx
+++ src/halamanku/homepage.tsx
@@ -17,13 +17,13 @@ export const Homepageku=()=>{
             <div className="text-[#070F2B] font-bold text-[20px]"></div>
             <div className="flex gap-12 ">
                 <div className="py-8 ">
-                    <a className="text-[14px] text-white font-bold" href="">About</a>
+                    <a className="text-[14px] text-white font-bold p-2 hover:bg-green-500 rounded-lg" href="">About</a>
                 </div>
                 <div className="py-8 ">
-                    <a className="text-[14px] text-white font-bold" href="">My Works</a>
+                    <a className="text-[14px] text-white font-bold p-2 hover:bg-green-500 rounded-lg" href="">My Works</a>
                 </div>
                 <div className="py-8 ">
-                    <a className="text-[14px] text-white font-bold" href="">Contact</a>
+                    <a className="text-[14px] text-white font-bold p-2 hover:bg-green-500 rounded-lg" href="">Contact</a>
                 </div>
             </div>
 
@@ -37,16 +37,16 @@ export const Homepageku=()=>{
                 <h2 className="text-white text-[2rem] font-bold pb-8"> I am a Software Engineering for building Website and Mobile</h2>
                 <p className="text-white text-[18px] font-seXmibold py-2" >I always enjoy knowledge about Development Technolgy. <br /> Interesting for progamming Website Development and Mobile Development </p>
                 <div className="flex gap-4 flex-start my-8">
-                    <button className="bg-[#3b3dc9] p-[2rem] rounded-xl text-white font-semibold flex gap-2">
-                        <img className="w-5 h-5 leading-2" src={git} alt="" /> 
+                    <button className="bg-[#3b3dc9] p-8 rounded-xl text-white font-semibold flex justify-around items-center">
+                        <img className="w-5 h-5" src={git} alt="" /> 
                         <p className="">Github</p>
                     </button>
-                    <button className="bg-[#47c93b] p-8 rounded-xl text-white font-semibold flex gap-2">
-                        <img className="w-6 h-6 leading-6" src={discord} alt="" /> 
+                    <button className="bg-[#47c93b] p-8 rounded-xl text-white font-semibold flex justify-around items-center">
+                        <img className="w-6 h-6" src={discord} alt="" /> 
                         <p className="">Discord</p>
                     </button>
-                    <button className="bg-[#c93b55] p-[2rem] rounded-xl text-white font-semibold flex gap-2">
-                        <img className="w-6 h-6 leading-6" src={insta} alt="" /> 
+                    <button className="bg-[#c93b55] p-[2rem] rounded-xl text-white font-semibold flex justify-around items-center">
+                        <img className="w-6 h-6 " src={insta} alt="" /> 
                         <p className="">Instagram</p>
                     </button>
                 </div>
@@ -54,50 +54,52 @@ export const Homepageku=()=>{
             <img className=" w-[20rem] h-auto border-auto rounded-lg mb-[2rem] mr-[8-rem] p-[4rem]" src= {gambar} />
 
         </div>
-        <div className="bg-gradient-to-r from-slate-500 to-slate-800 w-full flex flex-col">
-            <div className="flex justify-center">
-                <h2 className="text-white font-bold text-[2rem]">Tools And Skills</h2>
-            </div>
-            <div className="flex flex-row mx-4 justify-center gap-4 flex-wrap">
+
+        <div className="bg-gradient-to-r from-slate-500 to-slate-800 w-full pb-8">
+            <h2 className="text-white font-bold text-[2rem] text-center py-8">Tools And Skills</h2>
+            <div className="flex flex-row mx-4 justify-center gap-12 flex-wrap">
                 <div className="bg-white">
-                    <div className="border-2 border-solid border-gray-500 w-[15rem] pb-4">
-                        <img className="" src={htmlku} alt="" />
+                    <div className="w-[15rem] pb-4">
+                        <img className="w-[310px] h-[150px]" src={htmlku} />
+                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">HTML</p>
+                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about Html</p>
+                    </div>
+                </div>
+
+                <div className="bg-white">
+                    <div className="w-[15rem] pb-4">
+                        <img className="w-[320px] h-[150px]" src={cssku} alt="" />
+                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">CSS</p>
+                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about CSS</p>
+                    </div>
+                </div>
+
+                <div className="bg-white">
+                    <div className="w-[15rem] pb-4">
+                        <img className="w-[300px] h-[150px]" src={rect} alt="" />
+                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">REACT JS</p>
+                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about ReactJs</p>
+                    </div>
+                </div>
+
+                <div className="bg-white">
+                    <div className="w-[15rem] pb-4">
+                        <img className="w-[300px] h-[150px]" src={js} alt="" />
                         <p className="text-lg text-black font-bold text-center mb-4 pt-4">JAVASCRIPT</p>
                         <p className="text-sm text-gray-700 font-normal text-center">Understanding about Javascript</p>
                     </div>
                 </div>
 
                 <div className="bg-white">
-                    <div className="border-2 border-solid border-gray-500 w-[15rem] pb-4">
-                        <img className="" src={cssku} alt="" />
-                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">JAVASCRIPT</p>
-                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about Javascript</p>
+                    <div className="w-[15rem] pb-4">
+                        <img className="w-[300px] h-[150px]" src={node} alt="" />
+                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">NODE JS</p>
+                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about NodeJs</p>
                     </div>
                 </div>
 
-                <div className="bg-white">
-                    <div className="border-2 border-solid border-gray-500 w-[15rem] pb-4">
-                        <img className="" src={js} alt="" />
-                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">JAVASCRIPT</p>
-                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about Javascript</p>
-                    </div>
-                </div>
-
-                <div className="bg-white">
-                    <div className="border-2 border-solid border-gray-500 w-[15rem] pb-4">
-                        <img className="" src={rect} alt="" />
-                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">JAVASCRIPT</p>
-                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about Javascript</p>
-                    </div>
-                </div>
-
-                <div className="bg-white">
-                    <div className="border-2 border-solid border-gray-500 w-[15rem] pb-4">
-                        <img className="" src={node} alt="" />
-                        <p className="text-lg text-black font-bold text-center mb-4 pt-4">JAVASCRIPT</p>
-                        <p className="text-sm text-gray-700 font-normal text-center">Understanding about Javascript</p>
-                    </div>
-                </div>
+                
+                
             </div>
         </div>
         </>    
